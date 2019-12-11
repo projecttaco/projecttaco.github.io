33ea94f66c3df9ff2dc2ef66db4e22075f475cc1
Pensivej <jshim10@illinois.edu>
2019-12-12 00:10:25 +0900

    added test

---
+++ test/precache-manifest.ed5ca1a85e284f338a32c13cdd49a389.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df7391f145e26e6f63b7",
+    "url": "/test/static/css/main.c1119213.chunk.css"
+  },
+  {
+    "revision": "df7391f145e26e6f63b7",
+    "url": "/test/static/js/main.df7391f1.chunk.js"
+  },
+  {
+    "revision": "da826b614507ef04d008",
+    "url": "/test/static/css/1.baff4597.chunk.css"
+  },
+  {
+    "revision": "da826b614507ef04d008",
+    "url": "/test/static/js/1.da826b61.chunk.js"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "dd3e1183f583eda5739bca036fd1a15c",
+    "url": "/test/index.html"
+  }
+];
